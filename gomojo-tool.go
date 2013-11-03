0ecedcc2505d67c07d7cbc192a06bb7330c72253
Manish Malik <manish@hazelmedia.in>
2013-11-03 16:00:26 +0530

    Ran go fmt

--- gomojo-tool.go
+++ gomojo-tool.go
@@ -6,7 +6,7 @@
 // I initially wrote bits of this for Instamojo integration with an app backend.
 // At this stage, this acts like a command-line API tool. The next step in the
 // process would be to integrate the other bits, and then abstract the
-// functionality into a Golang package and retain the command-line tool as a 
+// functionality into a Golang package and retain the command-line tool as a
 // program which uses the package library.
 
 // Currently Available actions:
@@ -14,7 +14,7 @@
 // auth, deauth, listoffers
 //
 // Example usage of the command-line API tool:
-// 
+//
 // gomojo-tool -action listoffers -app <your App-ID> -token <auth token>
 //
 // If you don't have a pre-generated Auth Token, you can either generate one first like this
@@ -24,32 +24,32 @@
 // Or you can simply pass the username and password as command line parameters
 // (note that this will generate an intermediate Auth Token, which will be destructed later)
 // I will later add functionality to read username and password from a file.
-// 
+//
 // gomojo-tool -action listoffers -app <your App-ID> -user <your username> -passwd <your password>
 //
 
 package main
 
 import (
-	"fmt"
-	"flag"
-	"net/http"
-	"io/ioutil"
-	"os"
 	"bytes"
 	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 type ListOffersResp struct {
-	Offers []Offer `json:"offers"`
-	Success bool `json:"status"`
+	Offers  []Offer `json:"offers"`
+	Success bool    `json:"status"`
 }
 
 type Offer struct {
-    ShortURL string `json:"shorturl"`
-    Title string `json:"title"`
-    Slug string `json:"slug"`
-    Status string `json:"status"`
+	ShortURL string `json:"shorturl"`
+	Title    string `json:"title"`
+	Slug     string `json:"slug"`
+	Status   string `json:"status"`
 }
 
 var cmd_action, cmd_app_id, cmd_auth_token, cmd_username, cmd_passwd, cmd_api_ver string
@@ -62,19 +62,19 @@ var authenticated_in_current bool
 // or want to instead use the existing available parsing packages.
 func init() {
 
-		flag.StringVar(&cmd_action, "action", "", "API Action")
-        flag.StringVar(&cmd_app_id, "app", "", "App ID")
-        flag.StringVar(&cmd_auth_token, "token", "", "Auth Token")
-        flag.StringVar(&cmd_username, "user", "", "Username (for Auth)")
-        flag.StringVar(&cmd_passwd, "passwd", "", "Password (for Auth)")
-        flag.StringVar(&cmd_api_ver, "version", "1", "API Version (default 1)")
+	flag.StringVar(&cmd_action, "action", "", "API Action")
+	flag.StringVar(&cmd_app_id, "app", "", "App ID")
+	flag.StringVar(&cmd_auth_token, "token", "", "Auth Token")
+	flag.StringVar(&cmd_username, "user", "", "Username (for Auth)")
+	flag.StringVar(&cmd_passwd, "passwd", "", "Password (for Auth)")
+	flag.StringVar(&cmd_api_ver, "version", "1", "API Version (default 1)")
 
 }
 
 func initParams() {
 
 	var paramsOkay bool
-    paramsOkay = true
+	paramsOkay = true
 
 	flag.Parse()
 
@@ -90,42 +90,42 @@ func initParams() {
 	}
 
 	if !paramsOkay {
-                fmt.Print("Usage: gomojo-tool -action <Action> -app <App IP> [-token <Auth Token>] [-user <Username>] [-passwd <Password>]\n\n")
-                fmt.Print("Currently Available actions: auth, deauth, listoffers\n")
-                fmt.Print("Example: gomojo-tool -action listoffers -app <your App-ID> -token <auth token>\n")
-                fmt.Print("Example: gomojo-tool -action listoffers -app <your App-ID> -user <your username> -passwd <your password>\n")
-                os.Exit(1)
-        }
+		fmt.Print("Usage: gomojo-tool -action <Action> -app <App IP> [-token <Auth Token>] [-user <Username>] [-passwd <Password>]\n\n")
+		fmt.Print("Currently Available actions: auth, deauth, listoffers\n")
+		fmt.Print("Example: gomojo-tool -action listoffers -app <your App-ID> -token <auth token>\n")
+		fmt.Print("Example: gomojo-tool -action listoffers -app <your App-ID> -user <your username> -passwd <your password>\n")
+		os.Exit(1)
+	}
 
 }
 
 func filterOutputAPI(apicall string) {
 
 	api_result := callAPI(apicall)
-	
+
 	if apicall == "listoffers" {
 
 		jsonobj := new(ListOffersResp)
-    	jsonerr := json.Unmarshal([]byte(api_result), jsonobj)
-		
+		jsonerr := json.Unmarshal([]byte(api_result), jsonobj)
+
 		if jsonerr != nil {
-			fmt.Println("Invalid JSON: ",jsonerr.Error())
+			fmt.Println("Invalid JSON: ", jsonerr.Error())
 			os.Exit(3)
 		} else {
-			fmt.Printf("Total %d Offers\n",len(jsonobj.Offers))
+			fmt.Printf("Total %d Offers\n", len(jsonobj.Offers))
 			fmt.Println("----------------------------")
 
 			for iter := range jsonobj.Offers {
 				offer := jsonobj.Offers[iter]
-				fmt.Println("Offer #",iter+1)
-				fmt.Println("Status: ",offer.Status)
-				fmt.Println("Title: ",offer.Title)
-				fmt.Println("Slug: ",offer.Slug)
-				fmt.Println("ShortURL: ",offer.ShortURL)
+				fmt.Println("Offer #", iter+1)
+				fmt.Println("Status: ", offer.Status)
+				fmt.Println("Title: ", offer.Title)
+				fmt.Println("Slug: ", offer.Slug)
+				fmt.Println("ShortURL: ", offer.ShortURL)
 				fmt.Println("----------------------------")
-			}	
+			}
 		}
-		
+
 	} else {
 		fmt.Println(api_result)
 	}
@@ -152,7 +152,7 @@ func callAPI(apicall string) string {
 	}
 
 	api_result := ""
-	client := &http.Client{ }
+	client := &http.Client{}
 	api_method := "GET" // overridden later
 	var param_data []byte
 	var param_reader *bytes.Reader
@@ -174,17 +174,17 @@ func callAPI(apicall string) string {
 
 	// Make the API URL to call
 	api_url := "https://www.instamojo.com/api/" + cmd_api_ver + "/" + apicall + "/"
-	
+
 	param_reader = bytes.NewReader(param_data)
 
 	req, err := http.NewRequest(api_method, api_url, param_reader)
 	if err == nil {
 		req.Header.Add("X-App-Id", cmd_app_id)
-		
-		if (apicall != "auth") {
+
+		if apicall != "auth" {
 			req.Header.Add("X-Auth-Token", cmd_auth_token)
 		}
-		
+
 		resp, resperr := client.Do(req)
 		if resperr != nil {
 			fmt.Print("Error connecting to or retrieving response from API URL. Please check connectivity.\n")
@@ -193,19 +193,19 @@ func callAPI(apicall string) string {
 		}
 
 		defer resp.Body.Close()
-	
+
 		bodybytes, _ := ioutil.ReadAll(resp.Body)
 
 		api_result = string(bodybytes)
 	}
 
-	return api_result;
+	return api_result
 }
 
 func main() {
 
 	initParams()
-	
+
 	filterOutputAPI(cmd_action)
 
 	if authenticated_in_current {
